31f73eca5e440e74bb216e497ca0b11829941204
Jorge Murillo <j.orlandomurillo@gmail.com>
2018-02-08 17:22:07 +0800

    max counters

--- maxCounters.py
+++ maxCounters.py
@@ -1,4 +1,11 @@
 def solution(N, A):
+    arr = [0] * len(A)
+    num = N
 
+    for x in A:
+        if 1 <= x <= N:
+            arr[x] += 1
+    
+    return arr
 
 print(solution(5, [3,4,4,6,1,4,4]))
