3e426c11fa2f4e309855e23591870cf04a8e9dbc
Jorge Murillo <j.orlandomurillo@gmail.com>
2018-02-23 16:44:16 +0800

    updates

---
+++ genomicRangeQuery.py
@@ -0,0 +1,17 @@
+def solution(S, P, Q):
+    dic = {
+            'A': 1,
+            'C': 2,
+            'G': 3,
+            'T': 4
+        }
+
+    count = 0
+    arr = []
+
+    for x in range(P[count], Q[count]):
+
+    return arr
+
+
+print(solution(['C','A','G','C','C','T','A'], [2,5,0], [4,5,6]))
--- test.py
+++ test.py
@@ -1,15 +1,35 @@
-def solution(A):
-    arr = [0] * (len(A))
+class BinT():
+    def __init__(self, value):
+        self.value = value
+        self.left_node = None
+        self.right_node = None
+    
+    def insert_left(self, value):
+        if self.left_node:
+            self.left_node.value = value
+        else:
+            temp = BinT(value)
+            temp.left_node = self.left_node
+            self.left_node = temp
 
-    for x in A:
-        if 1 <= x <= len(A):
-            arr[x-1] = 1
+    def insert_right(self, value):
+        if self.right_node:
+            self.right_node.value = value
+        else:
+            temp = BinT(value)
+            temp.right_node = self.right_node
+            self.right_node = temp
 
-    for y in range(len(arr)):
-        if arr[y] == 0:
-            return y+1
+    def pre_order(self):
+        print(self.value)
 
-    return -1
+        if self.left_node:
+            self.left_node.pre_order()
+        if self.right_node:
+            self.right_node.pre_order()
 
-print(solution([1, 3, 6, 4, 1, 2]))
-print(solution([-1, -3]))
+hello = BinT(6)
+hello.insert_right(1)
+hello.insert_left(2)
+
+hello.pre_order()
